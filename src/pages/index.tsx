65446dc6c65aac842f51459eb0f7dae00cd25976
aly3n <aly3n.dev@gmail.com>
2022-02-05 02:08:51 +0330

    some head change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,16 @@ import ListPage from "../components/listPage/ListPage";
 export default function list(): JSX.Element {
   return (
     <Fragment>
-      {/* <Head>
+      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {careerMeta.preload.map((src) => (
+        {/* {careerMeta.preload.map((src) => (
           <link key={src} rel="preload" as="image" href={src} />
-        ))}
-        <title>{careerMeta.title}</title>
-        <meta name="description" content={careerMeta.description} />
-        <meta name="keywords" content={careerMeta.keywords} />
-        <meta name="author" content={careerMeta.author} />
-      </Head> */}
+        ))} */}
+        <title>محصولات فرخی</title>
+        <meta name="description" content={"جنسای بنجول ، آهن پاره و غیره"} />
+        {/* <meta name="keywords" content={careerMeta.keywords} />
+        <meta name="author" content={careerMeta.author} /> */}
+      </Head>
         <ListPage />
     </Fragment>
   );
